85c3a91d50cac0f8aa8f7aa191a5d29b8cf0640d
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-03-07 15:42:32 +0000

    Add Comment a nd comment-box dependence

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Comment extends React.Component {
+  render() {
+    return (
+      <div className="comment">
+        <p className="comment-header">Anne Droid</p>
+        <p className="comment-body">I wanna know what love is...</p>
+        <div className="comment-footer">
+          <a href="#" className="comment-footer-delete">
+            Delete comment
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+class CommentBox extends React.Component {
+  render() {
+    return (
+      <div className="comment-box">
+        <h4 className="comment-count">2 comments</h4>
+        <div className="comment-list">
+          <Comment author="Morgan McCircuit" body="Great picture!" />
+          <Comment author="Bending Bender" body="Excellent stuff" />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<CommentBox />, document.getElementById("comment-box1"));
